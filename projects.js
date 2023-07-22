876d49dfb4d4a88bbc65098de9c4b1d7465316c3
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-23 00:03:20 +0300

    finish 03-rotating-navigation javascript and add project to land page

--- projects.js
+++ projects.js
@@ -1,7 +1,7 @@
 const projects = [
   { name: 'Expanding Cards', link: '01-expanding-cards' },
   { name: 'Progress Steps', link: '02-progress-steps' },
-  { name: 'Project 2', link: 'project2' },
+  { name: 'Rotating Navigation', link: '03-rotating-navigation' },
   { name: 'Project 2', link: 'project2' },
   { name: 'Project 2', link: 'project2' },
   { name: 'Project 2', link: 'project2' },
