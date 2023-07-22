ee125b279180f712ac6f0092ada08ac166424d4f
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-22 18:50:04 +0300

    add project 2

--- projects.js
+++ projects.js
@@ -1,6 +1,6 @@
 const projects = [
   { name: 'Expanding Cards', link: '01-expanding-cards' },
-  { name: 'Project 2', link: 'project2' },
+  { name: 'Progress Steps', link: '02-progress-steps' },
   { name: 'Project 2', link: 'project2' },
   { name: 'Project 2', link: 'project2' },
   { name: 'Project 2', link: 'project2' },
