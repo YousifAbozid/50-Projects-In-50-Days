5b35d0b791873f90542403a79940ea7062d01beb
Yousif Abozid <yousif.abozid@yahoo.com>
2023-07-23 11:44:35 +0300

    add 04-hidden-search

--- projects.js
+++ projects.js
@@ -2,10 +2,7 @@ const projects = [
   { name: 'Expanding Cards', link: '01-expanding-cards' },
   { name: 'Progress Steps', link: '02-progress-steps' },
   { name: 'Rotating Navigation', link: '03-rotating-navigation' },
-  { name: 'Project 2', link: 'project2' },
-  { name: 'Project 2', link: 'project2' },
-  { name: 'Project 2', link: 'project2' },
-  { name: 'Project 2', link: 'project2' },
+  { name: 'Hidden Search', link: '04-hidden-search' },
 ]
 
 export default projects
